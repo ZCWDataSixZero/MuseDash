8d1c8309707b14c3bc4e6f0e72ab777dad7e0d30
KunleAdeyanju <aoadeyanju22@gmail.com>
2025-05-07 14:05:28 -0400

    uploading my streamlit demo

---
+++ Resources/MusicDB.py
@@ -0,0 +1,90 @@
+import pandas as pd
+import pyspark
+from pyspark.sql.functions import countDistinct, count, col, avg, max, min, countDistinct, sum, round, desc
+
+
+# class MusicDB():
+    
+#     def __init__(self):
+#         pass
+
+
+def get_artist_state_listen( df: pyspark.sql.dataframe.DataFrame , artist: str) -> pyspark.sql.dataframe.DataFrame:
+    '''
+    Filters and aggregates a pyspark dataframe to count listens by artist and state
+
+    Args:
+        df (pyspark.sql.dataframe.DataFrame): dataframe
+        artist (str): name of the artist
+
+    Returns:
+        filtered and aggregated dataframe 
+    
+    '''
+    df = df.groupBy('artist','state').agg(count('*').alias('listens')).where(col('artist') == artist).orderBy(desc('listens'))
+    return df
+
+def get_arist_over_1000(df: pyspark.sql.dataframe.DataFrame, number_of_lis) -> list:
+    '''
+    Takes in a pyspark dataframe and returns list of artists with at least a states number of listens
+
+    Args:
+        df (pyspark.sql.dataframe.DataFrame): dataframe
+        number_of_lis (int): min number of listens
+
+    Returns:
+        list: number of artists with at least the specified number of listens
+
+    '''
+    df = df.groupBy('artist').agg(count('*').alias('listens')).filter(col('listens') >= number_of_lis)
+    df_list = [data[0] for data in df.select('artist').collect()]
+    return df_list
+
+def map_prep_df(df: pyspark.sql.dataframe.DataFrame) -> pd.core.frame.DataFrame:
+    '''
+    Takes a filtered pyspark dataframe and returns a pandas dataframe with state names 
+
+    Arg:
+        df (pyspark.sql.dataframe.DataFrame): dataframe
+
+    Returns:
+        pandas dataframe: dataframe of artist, # of listens, US states: name & abr
+
+    '''
+    us_state_to_abbrev = {
+    "Alabama": "AL", "Alaska": "AK", "Arizona": "AZ", "Arkansas": "AR",
+    "California": "CA", "Colorado": "CO", "Connecticut": "CT", "Delaware": "DE",
+    "Florida": "FL", "Georgia": "GA", "Hawaii": "HI", "Idaho": "ID", "Illinois": "IL",
+    "Indiana": "IN", "Iowa": "IA", "Kansas": "KS", "Kentucky": "KY", "Louisiana": "LA",
+    "Maine": "ME", "Maryland": "MD", "Massachusetts": "MA", "Michigan": "MI", "Minnesota": "MN",
+    "Mississippi": "MS", "Missouri": "MO", "Montana": "MT", "Nebraska": "NE", "Nevada": "NV",
+    "New Hampshire": "NH", "New Jersey": "NJ", "New Mexico": "NM", "New York": "NY", "North Carolina": "NC",
+    "North Dakota": "ND", "Ohio": "OH", "Oklahoma": "OK", "Oregon": "OR", "Pennsylvania": "PA",
+    "Rhode Island": "RI", "South Carolina": "SC", "South Dakota": "SD", "Tennessee": "TN",
+    "Texas": "TX", "Utah": "UT", "Vermont": "VT", "Virginia": "VA", "Washington": "WA",
+    "West Virginia": "WV", "Wisconsin": "WI", "Wyoming": "WY", "District of Columbia": "DC",
+    }
+    
+    us_states = list(us_state_to_abbrev.items())
+    us_states_columns = ['NAME', 'state']  # <-- make sure this matches
+    states_df = pd.DataFrame(us_states, columns=us_states_columns)
+
+    df = df.toPandas()
+    artist = df.artist[0]
+
+    map_prep = pd.merge(
+    left = df,
+    right = states_df,
+    left_on = 'state',
+    right_on ='state',
+    how = 'right')
+
+    map_prep.listens = map_prep.listens.fillna(0)
+    map_prep.artist = map_prep.artist.fillna(artist)
+    
+    return map_prep
+
+
+def top_5(df: pyspark.sql.dataframe.DataFrame) ->  pyspark.sql.dataframe.DataFrame:
+    df = df.orderBy(desc('listens')).limit(5)
+    return df
---
+++ Resources/streamlitdemo.py
@@ -0,0 +1,51 @@
+import streamlit as st
+import numpy as np
+import plotly.express as px
+import MusicDB
+import plotly.graph_objects as go
+
+# fig = px.choropleth(locations=["CA", "TX", "NY",'AK'], locationmode="USA-states", color=[1,2,3,4], scope="usa")
+
+
+# st.plotly_chart(fig)
+
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+    .appName("Museh PySpark Learning") \
+        .getOrCreate()
+
+## Verify that SparkSession is created
+
+try:
+    df_listen = spark.read.json ('./Data/listen_events')
+    print('Data loaded successfully')
+except Exception as e:
+    print(f'Error loading data: {e}')
+
+
+b = MusicDB.get_artist_state_listen(df=df_listen, artist='Aerosmith')
+
+c = MusicDB.map_prep_df(df=b)
+
+fig = go.Figure(data=go.Choropleth(
+    locations=c.state, # Spatial coordinates
+    z = c.listens, # Data to be color-coded
+    locationmode = 'USA-states', # set of locations match entries in `locations`
+    colorscale = 'Reds',
+    colorbar_title = "listens",
+))
+
+fig.update_layout(
+    title_text = '2011 US Agriculture Exports by State',
+    geo_scope='usa', # limite map scope to USA
+)
+
+tab1, tab2 = st.tabs(["Streamlit theme (default)", "Plotly native theme"])
+
+with tab1:
+    st.plotly_chart(fig)
+
+with tab2:
+    if st.button("Send balloons!"):
+        st.balloons()
