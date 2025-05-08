b5162bd2ef7304e4ed6c846fc0f0decbfcd46743
KunleAdeyanju <aoadeyanju22@gmail.com>
2025-05-08 13:23:20 -0400

    rename MusicDB to engine.py to properly make sense

--- Resources/Kunle/streamlitdemo.py
+++ Resources/Kunle/streamlitdemo.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import plotly.express as px
-import MusicDB
+import engine
 import plotly.graph_objects as go
 
 # fig = px.choropleth(locations=["CA", "TX", "NY",'AK'], locationmode="USA-states", color=[1,2,3,4], scope="usa")
@@ -26,7 +26,7 @@ except Exception as e:
 
 
 # list of artists with a certain number
-artist_list = MusicDB.get_arist_over_1000(df=df_listen,number_of_lis=1000)
+artist_list = engine.get_arist_over_1000(df=df_listen,number_of_lis=1000)
 
 
 #selected_artist = st.sidebar.selectbox()
@@ -39,6 +39,8 @@ tab1, tab2 = st.tabs(["Map", "2nd for giggles"])
 
 with tab1:
     with st.container():
+
+
         option = st.selectbox(
         'Select an Artist',
         artist_list,
@@ -54,10 +56,10 @@ with tab1:
         pass
     else:
         # creating the dataframe of listens for specific artists
-        b = MusicDB.get_artist_state_listen(df=df_listen, artist=option)
+        b = engine.get_artist_state_listen(df=df_listen, artist=option)
 
         # filtering data to what is needed to make map
-        c = MusicDB.map_prep_df(df=b)
+        c = engine.map_prep_df(df=b)
         ## creating the maps
         fig = go.Figure(data=go.Choropleth(
             locations=c.state, # Spatial coordinates
