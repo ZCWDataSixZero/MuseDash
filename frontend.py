56b58b6ffc5bdf5f601370f9f3fc338c3ab98444
jwheller0013 <jwheller0013@yahoo.com>
2025-05-09 16:31:08 -0400

    Changes to layout

--- frontend.py
+++ frontend.py
@@ -8,6 +8,9 @@ import altair as alt
 
 from pyspark.sql import SparkSession
 
+# makes page wide
+st.set_page_config(layout = 'wide')
+
 spark = SparkSession.builder \
     .appName("MuseDash PySpark") \
         .getOrCreate()
@@ -15,7 +18,7 @@ spark = SparkSession.builder \
 ## Verify that SparkSession is created
 
 try:
-    df_listen = spark.read.json ('/Users/isiah/Downloads/Data/listen_events')
+    df_listen = spark.read.json ('/Users/jim/Projects/p1/spring25data/Data/listen_events')
     print('Data loaded successfully')
 except Exception as e:
     print(f'Error loading data: {e}')
@@ -24,98 +27,108 @@ except Exception as e:
 cleaned_listen = engine.clean(df=df_listen)
 artist_list = engine.get_artist_over(df=df_listen,number_of_lis=1000)
 
-# makes page wide
-st.set_page_config(layout = 'wide')
+# # allow .css formatting
+# def local_css(file_name):
+#     with open(file_name) as f:
+#         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-# allow .css formatting
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-
-local_css("style.css")
+# local_css("style.css")
                    
 # Streamlit Titling
 st.title("Muse Dash")
 
-
-#creating tabs
-tab1, tab2 = st.tabs(["Map", "2nd for giggles"])
-
-with tab1:
      
-     with st.container():
+with st.container():
 
-        col_table = st.columns((5, 1, 5), gap='medium')
+    col_table = st.columns((5, 10), gap='medium')
+    
+    # Sidebar
+    st.sidebar.header("Select a State")
+    available_states = engine.get_states_list(cleaned_listen)
+    selected_state = st.sidebar.selectbox("Filter by State (Optional):", 
+                                        ['Nationwide'] + available_states,
+                                        )
+    # titles depending on state selected
+    if selected_state == 'Nationwide':
+        top_10_header = "Top 10 National Artists"
+        pie_title = "National Subscription Type Distribution"
+        paid_title = 'Top Songs for Paid Users'
+        free_title = 'Top Songs for Free Users'
+    else:
+        top_10_header = f"Top 10 Artists in {selected_state}"
+        pie_title = f"Subscription Type Distribution in {selected_state}"
+        paid_title = f'Top Songs for Paid Users in {selected_state}'
+        free_title = f'Top Songs for Free Users in {selected_state}'
+
+
+    with col_table[0]:
+        # printing top ten chart
+        top_10 = engine.get_top_10_artists(df=cleaned_listen, state=selected_state)
+        st.header(top_10_header)
+        st.dataframe(top_10, hide_index=True)
+
+    
+    with col_table[0]:
+        # printing pie
+        pie_df = engine.create_subscription_pie_chart(df=cleaned_listen, state=selected_state)
+
+
+        chart = alt.Chart(pie_df).mark_arc().encode(
+        theta=alt.Theta(field="count", type="quantitative"),
+        color=alt.Color(field="subscription", type="nominal",
+                        scale=alt.Scale(domain=['free', 'paid'],
+                                        range=['orange', 'blue']),
+                        legend=alt.Legend(title="Subscription Type", orient="bottom")),
+        order=alt.Order(field="count", sort="descending"),
+        tooltip=["subscription", "count"]
+    ).properties(
+        title=pie_title
+    ).configure_view(
+        fillOpacity=0  # Make the chart background transparent
+    )
+        st.altair_chart(chart)
         
-        # Sidebar
-        st.sidebar.header("Select a State")
-        available_states = engine.get_states_list(cleaned_listen)
-        selected_state = st.sidebar.selectbox("Filter by State (Optional):", 
-                                            ['Nationwide'] + available_states,
-                                            )
-        # titles depending on state selected
-        if selected_state == 'Nationwide':
-            top_10_header = "Top 10 National Artists"
-            pie_title = "National Subscription Type Distribution"
-            paid_title = 'Top Songs for Paid Users'
-            free_title = 'Top Songs for Free Users'
-        else:
-            top_10_header = f"Top 10 Artists in {selected_state}"
-            pie_title = f"Subscription Type Distribution in {selected_state}"
-            paid_title = f'Top Songs for Paid Users in {selected_state}'
-            free_title = f'Top Songs for Free Users in {selected_state}'
+    with col_table[1]:
+        with st.container():
 
-
-        with col_table[0]:
-            # printing top ten chart
-            top_10 = engine.get_top_10_artists(df=cleaned_listen, state=selected_state)
-            st.header(top_10_header)
-            st.dataframe(top_10, hide_index=True)
-
-        
-        with col_table[0]:
-            # printing pie
-            pie_df = engine.create_subscription_pie_chart(df=cleaned_listen, state=selected_state)
-
-
-            chart = alt.Chart(pie_df).mark_arc().encode(
-            theta=alt.Theta(field="count", type="quantitative"),
-            color=alt.Color(field="subscription", type="nominal",
-                            scale=alt.Scale(domain=['free', 'paid'],
-                                            range=['red', 'green']),
-                            legend=alt.Legend(title="Subscription Type", orient="bottom")),
-            order=alt.Order(field="count", sort="descending"),
-            tooltip=["subscription", "count"]
-        ).properties(
-            title=pie_title
-        ).configure_view(
-            fillOpacity=0  # Make the chart background transparent
+            # st.sidebar.header("Select an Artist")
+            option = st.selectbox(
+            'Filter by Artist',
+            artist_list,
+            index=None,
+            placeholder="Chosen Artist",
+            accept_new_options = True
         )
-            st.altair_chart(chart)
-            
 
-        with col_table[2]:
-            # listen graph creation
-            listen_duration = engine.get_user_list(df=cleaned_listen, state=selected_state)
 
-            # Determine the title based on the selected state
-            if selected_state == "Nationwide":
-                chart_title = "How long are users listening in the USA?"
-            else:
-                chart_title = f"How long are users listening in {selected_state}?"
-                
-            #create the line graph
-            line_fig = px.line(
-                listen_duration,
-                x="month_name",
-                y="total_duration",
-                color="subscription",
-                title=chart_title,
-                labels={"month_name": "Month", "total_duration": "Total Duration (seconds)"}
-                    )
-            st.plotly_chart(line_fig)
-        
-        with col_table[2]:
+        if option == None:
+            st.write("You selected: ", option)
+        else:
+            # creating the dataframe of listens for specific artists
+            b = engine.get_artist_state_listen(df=cleaned_listen, artist=option)
+
+            # filtering data to what is needed to make map
+            c = engine.map_prep_df(df=b)
+            ## creating the maps
+            fig = go.Figure(data=go.Choropleth(
+                locations=c.state, # Spatial coordinates
+                z = c.listens, # Data to be color-coded
+                locationmode = 'USA-states', # set of locations match entries in `locations`
+                colorscale = 'Blues',
+                colorbar_title = "Number of\n Listens"
+            ))
+
+            # adding context to the map
+            fig.update_layout(
+                title_text = f'Number of {option} Listens \n 2024-2025',
+                geo_scope='usa', # limit map scope to USA
+            )
+
+            st.plotly_chart(fig)
+    
+    with col_table[1]:
+        col_free, col_paid, col_line = st.columns(3)
+        with col_paid:
             # paid songs charts
             st.subheader(paid_title)
             paid_songs_df = engine.top_paid_songs(df=cleaned_listen, state=selected_state)
@@ -129,9 +142,9 @@ with tab1:
                 height=400
             )
             st.altair_chart(chart_paid_songs, use_container_width=True)            
-        
-        with col_table[0]:
-            # free songs chart
+    
+        with col_free:
+        # free songs chart
             st.subheader(free_title)
             free_songs_df = engine.top_free_songs(df=cleaned_listen, state=selected_state)
             
@@ -144,43 +157,24 @@ with tab1:
                 height=400
             )
             st.altair_chart(chart_free_songs, use_container_width=True)
+        
+            with col_line:
+                # listen graph creation
+                listen_duration = engine.get_user_list(df=cleaned_listen, state=selected_state)
 
-
-
-with tab2:
-    with st.container():
-
-        # st.sidebar.header("Select an Artist")
-        option = st.selectbox(
-        'Filter by Artist',
-        artist_list,
-        index=None,
-        placeholder="Chosen Artist",
-        accept_new_options = True
-    )
-
-
-    if option == None:
-        st.write("You selected: ", option)
-    else:
-        # creating the dataframe of listens for specific artists
-        b = engine.get_artist_state_listen(df=cleaned_listen, artist=option)
-
-        # filtering data to what is needed to make map
-        c = engine.map_prep_df(df=b)
-        ## creating the maps
-        fig = go.Figure(data=go.Choropleth(
-            locations=c.state, # Spatial coordinates
-            z = c.listens, # Data to be color-coded
-            locationmode = 'USA-states', # set of locations match entries in `locations`
-            colorscale = 'Blues',
-            colorbar_title = "Number of\n Listens"
-        ))
-
-        # adding context to the map
-        fig.update_layout(
-            title_text = f'Number of {option} Listens \n 2024-2025',
-            geo_scope='usa', # limit map scope to USA
-        )
-
-        st.plotly_chart(fig)
+                # Determine the title based on the selected state
+                if selected_state == "Nationwide":
+                    chart_title = "How long are users listening in the USA?"
+                else:
+                    chart_title = f"How long are users listening in {selected_state}?"
+                    
+                #create the line graph
+                line_fig = px.line(
+                    listen_duration,
+                    x="month_name",
+                    y="total_duration",
+                    color="subscription",
+                    title=chart_title,
+                    labels={"month_name": "Month", "total_duration": "Total Duration (seconds)"}
+                        )
+                st.plotly_chart(line_fig)
